c9dc51d26ce40fa5bf2387acf2dbf9da577eeb5d
Guhapriya01 <guhapriyadharmaraj08@gmail.com>
2024-03-25 16:56:35 +0530

    uppdate

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -6,6 +6,14 @@ import { tracked } from '@glimmer/tracking';
 export default class NavBarComponent extends Component {
     @service data;
     @service userPreference;
+    @tracked theme;
+
+    constructor(){
+        super(...arguments);
+        let value = get(this.userPreference,'theme') ;
+        this.theme = value ? value : 'light';
+        this.changeColour();
+    }
 
     get libraryReq(){
          return this.data.libraryReq;
@@ -15,22 +23,34 @@ export default class NavBarComponent extends Component {
         return this.data.booksReq;
     }
 
-    get theme(){
-        let t = get(this.userPreference,'theme');
+    // get theme(){
+    //     console.log("getter");
+        
+    //     let t = get(this.userPreference,'theme');
+    //     // setting body colour
+        // let backgroundColour = t == 'dark' ? 'grey' : 'white';
+        // let colour = t == 'dark' ? 'white' : 'black';
+        // $('body').css('background-color', backgroundColour);
+        // $('body').css('color', colour);
 
-        // setting body colour
-        let backgroundColour = t == 'dark' ? 'grey' : 'white';
-        let colour = t == 'dark' ? 'white' : 'black';
-        $('body').css('background-color', backgroundColour);
-        $('body').css('color', colour);
-
-        return t;
-    }
+    //     // return t;
+    //     return get(this.userPreference,'theme');
+    // }
 
     @action
     changeTheme(){
-        let t= this.theme == 'dark' ? 'light' : 'dark';
+        let t = this.theme == 'dark' ? 'light' : 'dark';
+        this.theme = t;
+        this.changeColour();
         set(this.userPreference, 'theme' , t);
+    }
+
+    @action
+    changeColour(){
+        let backgroundColour = this.theme  == 'dark' ? 'grey' : 'white';
+        let colour = this.theme  == 'dark' ? 'white' : 'black';
+        $('body').css('background-color', backgroundColour);
+        $('body').css('color', colour);
     }
 
 }
--- app/services/user-preference.js
+++ app/services/user-preference.js
@@ -3,25 +3,27 @@ import Service from '@ember/service';
 export default class UserPreferenceService extends Service {
 
     set theme(newTheme) {
-        let date = new Date();
-
+        // let date = new Date();
         // seetting expiry date of 30 days
-        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
-        document.cookie = `theme=${newTheme};expires=${date.toUTCString()}`;
+        // date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
+        // document.cookie = `theme=${newTheme};expires=${date.toUTCString()}`;
+
+        localStorage.setItem("theme",newTheme)
     }
 
     get theme() {
-        let cookieArr = document.cookie.split('; ');
-        let t = 'light';
-        if (cookieArr.length > 0) {
-            cookieArr.forEach((cookie) => {
-                let keyValue = cookie.split("=");
-                if (keyValue[0] == 'theme') {
-                    t = keyValue[1];
-                }
-            })
+        // let cookieArr = document.cookie.split('; ');
+        // let t = 'light';
+        // if (cookieArr.length > 0) {
+        //     cookieArr.forEach((cookie) => {
+        //         let keyValue = cookie.split("=");
+        //         if (keyValue[0] == 'theme') {
+        //             t = keyValue[1];
+        //         }
+        //     })
 
-        }
-        return t;
+        // }
+        // return t;
+        return localStorage.getItem("theme");
     }
 }

5f165de370a691f6a5421ca3c484cbc79993e783
null <null>
2024-03-25 10:01:38 +0530

    update

--- app/services/user-preference.js
+++ app/services/user-preference.js
@@ -4,13 +4,16 @@ export default class UserPreferenceService extends Service {
 
     set theme(newTheme){
         let date = new Date();
+
+        // seetting expiry date of 30 days
         date.setTime( date.getTime() + (30 * 24 * 60 * 60 * 1000)); 
         document.cookie = `theme=${newTheme};expires=${date.toUTCString()}`;        
     }
 
     get theme(){
         let cookieArr = document.cookie.split('=');
-        if(cookieArr.length>0){
+        // console.log(cookieArr.length)
+        if(cookieArr.length>1){
             return cookieArr[1];
         }
         return 'light';

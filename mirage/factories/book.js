671fe0631e59c672b0ff4e598ac6e6e10daf3805
Guhapriya01 <guhapriyadharmaraj08@gmail.com>
2024-04-01 12:56:07 +0530

    Faker Update

--- mirage/factories/book.js
+++ mirage/factories/book.js
@@ -1,8 +1,9 @@
 import { Factory } from 'miragejs';
+import { faker } from '@faker-js/faker';
 
 export default Factory.extend({
-    name:(i)=>`Book ${i}`,
-    author: (i)=>`Author ${i}`,
-    publisher: (i)=>`Publisher ${i}`,
+    name:(i)=>`Book ${i+1}`,
+    author: ()=>faker.person.firstName(),
+    publisher: (i)=>`Publisher ${i+1}`,
     publishedDate: "2004-04-11",
 });
--- mirage/factories/library.js
+++ mirage/factories/library.js
@@ -1,8 +1,9 @@
 import { Factory ,association} from 'miragejs';
+import { faker } from '@faker-js/faker';
 
 export default Factory.extend({
-    name: (i) => `Library ${i}`,
-    location: (i) => `Location ${i}`,
+    name: (i) => `Library ${i+1}`,
+    location: () => faker.location.city(),
     timing: "10.00 am TO 5.00 pm",
     afterCreate(library, server) {
         library.update({
